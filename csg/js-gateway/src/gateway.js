05435a1ba1d49b1e010451818ed2975b5cad7cd5
Li Jun <lj110119@aliyun.com>
2021-04-07 16:19:34 +0800

    add mongodb support.

--- csg/js-gateway/src/gateway.js
+++ csg/js-gateway/src/gateway.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const app = express();
 const httpProxy = require('http-proxy');
-
-args = process.argv.splice(2)
-
 const apiProxy = httpProxy.createProxyServer();
-apiProxy.on('proxyRes', function(proxyRes, req, res){
-    proxyRes.on('data' , function(dataBuffer){
-        console.log("onData:", req.path, new Date())
-        let data = dataBuffer.toString('utf8');
-        console.log("This is the data from target server : "+ data);
-    }); 
+
+const args = process.argv.splice(2)
+const MongoClient = require('mongodb').MongoClient;
+const passwd = args[1] || "123456"
+const mongoUrl = "mongodb://linden:" + passwd + "@localhost:27017/flipped";
+const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let cids;
+client.connect(function(err, db) {
+    if (err) throw err;
+    cids = db.db("flipped").collection("cids")
 });
 
-app.all("/*", function(req, res) {
-    console.log("request:", req.path, args[0])
+apiProxy.on('proxyRes', function (proxyRes, req, res) {
+    proxyRes.on('data', function (dataBuffer) {
+        let str = dataBuffer.toString('utf8')
+        let json = JSON.parse(str)
+        json.api = req.path
+        json.createAt = new Date()
+        cids.insertOne(json)
+        console.log("res: " + JSON.stringify(json))
+    });
+});
+
+app.all("/*", function (req, res) {
+    console.log("req:", req.path, new Date())
     apiProxy.web(req, res, {
-        target: args[0]
+        target: args[0] || "http://localhost:5001"
     });
 });
 
