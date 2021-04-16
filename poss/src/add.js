591d85cf9a10c903db5470fb5e661999edc09864
Li Jun <lj110119@aliyun.com>
2021-04-16 10:46:16 +0800

    add moche test.

--- poss/src/add.js
+++ poss/src/add.js
@@ -4,7 +4,7 @@ const createClient = require('ipfs-http-client')
 const { globSource } = createClient
 
 module.exports = (options) => {
-  const url = options.url || 'http://poss.cipfs.cn/v1/add/' + options.clientID + '/api/v0'
+  const url = JSON.stringify(options) == "{}" ? {} : options.url || 'http://poss.cipfs.cn/v1/add/' + options.clientID + '/api/v0'
   const client = createClient(url)
   const addFile = async function (path) {
     const file = globSource(path)
