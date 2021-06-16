2b21a06bfd0f3038724042ad4e30133d66bb063a
Li Jun <lj110119@aliyun.com>
2021-06-16 17:53:37 +0800

    add orbitdb support

---
+++ myspace/src/dbHelper.js
@@ -0,0 +1,23 @@
+const createClient = require('ipfs-http-client')
+const OrbitDB = require('orbit-db')
+
+const clinet = createClient("http://csg.cipfs.cn/poss/v1/testdb");
+
+module.exports = async () => {
+    const orbitdb = await OrbitDB.createInstance(clinet);
+
+    const options = {
+        accessController: {
+            write: [
+                orbitdb.identity.id
+            ]
+        }
+    }
+
+    const db = await orbitdb.kvstore("dirInfo", options)
+    await db.load()
+
+    console.log(db.identity);
+    console.log(db.id);
+    return db
+}
---
+++ myspace/src/saveUserDir.js
@@ -0,0 +1,45 @@
+const createDB = require('./dbHelper');
+
+let dirInfo = {
+    "nocheck": true,
+    "type": "directory",
+    "name": "BACKUP",
+    "children": [
+      {
+        "nocheck": true,
+        "type": "directory",
+        "name": "hr-201.9-dbbackup",
+        "children": [
+          {
+            "nocheck": true,
+            "type": "file",
+            "name": "uxinhr.bak"
+          },
+          {
+            "nocheck": true,
+            "type": "file",
+            "name": "zknet10.bak"
+          }
+        ]
+      },
+      {
+        "nocheck": true,
+        "type": "file",
+        "name": "rsyncd - 副本.rar"
+      },
+      {
+        "nocheck": true,
+        "type": "file",
+        "name": "部署系统-websites-20160113.rar"
+      }
+    ]
+  }
+
+let db;
+(async () => {
+    db = await createDB()
+    dirInfo = db.get("user1");
+    console.log(dirInfo)
+
+    const hash = await db.put("user1", dirInfo);
+})()
