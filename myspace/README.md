5b96e55accd70c4b9056b4dbd82ed4ebcd91c49d
Li Jun <lj110119@aliyun.com>
2021-07-01 11:42:41 +0800

    update myspace README.md

--- myspace/README.md
+++ myspace/README.md
@@ -16,11 +16,72 @@ A IPFS-backed cloud disk.
 ## Install
 
 ```
+npm i linden-poss --save
+npm i orbit-db --save
+
+```
+
+## Initia
+vim dbHelper.js
+```
+const createClient = require('ipfs-http-client')
+const OrbitDB = require('orbit-db')
+
+const clinet = createClient("http://csg.cipfs.cn/poss/v1/testdb");
+
+module.exports = async () => {
+    const orbitdb = await OrbitDB.createInstance(clinet);
+
+    const options = {
+        accessController: {
+            write: [
+                // orbitdb.identity.id
+                "*"
+            ]
+        }
+    }
+
+    const db = await orbitdb.kvstore("dirInfo", options)
+    await db.load()
+
+    console.log(db.identity);
+    console.log(db.id);
+    return db
+}
+
 ```
 
 ## Usage
 
 ```
+const createDB = require('./dbHelper');
+
+let fileList = {
+  image: [
+    {
+      "Name": "8A4A1163.jpg",
+      "Hash": "QmWVLSaULcHpqzx7aSmVF4rtSZVmbC17DomYFVVR3oJ9QC",
+      "Size": "2837821"
+    },
+    {
+      "Name": "photo1.jpg",
+      "Hash": "QmPTMZ8pPnTNdaU3koBGxTPCr2d9YJPx3SrWgGw7T4A75G",
+      "Size": "3704828"
+    }
+
+  ],
+  video: [],
+  note: [],
+  file: []
+}
+let db;
+(async () => {
+  db = await createDB()
+  const hash = await db.put("user1", fileList);
+  dirInfo = db.get("user1");
+  console.log(dirInfo)
+})()
+
 ```
 
 ## Maintainers
--- myspace/src/saveUserDir.js
+++ myspace/src/saveUserDir.js
@@ -1,44 +1,63 @@
 const createDB = require('./dbHelper');
 
+let fileList = {
+  image: [
+    {
+      "Name": "8A4A1163.jpg",
+      "Hash": "QmWVLSaULcHpqzx7aSmVF4rtSZVmbC17DomYFVVR3oJ9QC",
+      "Size": "2837821"
+    },
+    {
+      "Name": "photo1.jpg",
+      "Hash": "QmPTMZ8pPnTNdaU3koBGxTPCr2d9YJPx3SrWgGw7T4A75G",
+      "Size": "3704828"
+    }
+
+  ],
+  video: [],
+  note: [],
+  file: []
+}
+
 let dirInfo = {
-    "nocheck": true,
-    "type": "directory",
-    "name": "BACKUP",
-    "children": [
-      {
-        "nocheck": true,
-        "type": "directory",
-        "name": "hr-201.9-dbbackup",
-        "children": [
-          {
-            "nocheck": true,
-            "type": "file",
-            "name": "uxinhr.bak"
-          },
-          {
-            "nocheck": true,
-            "type": "file",
-            "name": "zknet10.bak"
-          }
-        ]
-      },
-      {
-        "nocheck": true,
-        "type": "file",
-        "name": "rsyncd - 副本.rar"
-      },
-      {
-        "nocheck": true,
-        "type": "file",
-        "name": "部署系统-websites-20160113.rar"
-      }
-    ]
-  }
+  "nocheck": true,
+  "type": "directory",
+  "name": "BACKUP",
+  "children": [
+    {
+      "nocheck": true,
+      "type": "directory",
+      "name": "hr-201.9-dbbackup",
+      "children": [
+        {
+          "nocheck": true,
+          "type": "file",
+          "name": "uxinhr.bak"
+        },
+        {
+          "nocheck": true,
+          "type": "file",
+          "name": "zknet10.bak"
+        }
+      ]
+    },
+    {
+      "nocheck": true,
+      "type": "file",
+      "name": "rsyncd - 副本.rar"
+    },
+    {
+      "nocheck": true,
+      "type": "file",
+      "name": "部署系统-websites-20160113.rar"
+    }
+  ]
+}
 
 let db;
 (async () => {
-    db = await createDB()
-    const hash = await db.put("user1", dirInfo);
-    dirInfo = db.get("user1");
-    console.log(dirInfo)
+  db = await createDB()
+  const hash = await db.put("user1", fileList);
+  dirInfo = db.get("user1");
+  console.log(dirInfo)
 })()
