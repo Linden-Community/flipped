b065f46e796294b42f3e8417828ca6fd638b5894
lijun <lj110119@aliyun.com>
2022-01-27 09:43:36 +0800

    http 协议上传文件，机房安全设备会限制html文件，故修改成https.

--- poss/src/index.js
+++ poss/src/index.js
@@ -9,7 +9,7 @@ const globSource = require('./utils/glob-source')
 const urlSource = require('ipfs-utils/src/files/url-source')
 
 function create(options = {}) {
-    options.url = options.url || (options.clientID ? 'http://csg.lindensys.cn/poss/v1/' + options.clientID : null)
+    options.url = options.url || (options.clientID ? 'https://csg.lindensys.cn/poss/v1/' + options.clientID : null)
     const client = {
         add: require('./add')(options),
         get: require('./get')(options),
