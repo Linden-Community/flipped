0dda11c47b990a4ac915bb5acf06b67e1c648e6c
lijun <lj110119@aliyun.com>
2022-02-11 17:34:11 +0800

    linden-poss support v2.

--- poss/src/index.js
+++ poss/src/index.js
@@ -9,7 +9,8 @@ const globSource = require('./utils/glob-source')
 const urlSource = require('ipfs-utils/src/files/url-source')
 
 function create(options = {}) {
-    options.url = options.url || (options.clientID ? 'https://csg.lindensys.cn/poss/v1/' + options.clientID : null)
+    options.possVersion = options.possVersion || 'v1'
+    options.url = options.url || (options.clientID ? 'https://csg.lindensys.cn/poss/' + options.possVersion + '/' + options.clientID : null)
     const client = {
         add: require('./add')(options),
         get: require('./get')(options),
---
+++ poss/test/v2.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert').strict;
+const createClient = require('../src/index')
+
+describe('.add', function () {
+    this.timeout(60 * 1000)
+
+    let client
+
+    before(() => {
+        client = createClient.create({ clientID: "testnet", possVersion: "v2" })
+    })
+
+    it('add data', async () => {
+        let token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ilh3OTBZSTJCZGN4b3F4WFBwWHVvdndaUDNEc0Q0eEczczZ6bzFkcU0zTXMifQ.eyJqdGkiOiJFcFpjeHc1OHAyRzgtYmZuc1I1V0QiLCJpYXQiOjE2NDQ1NzE4MTAsImV4cCI6MTY0NTc4MTQxMCwic2NvcGUiOiJzdG9yZTp0ZXN0bmV0OioiLCJpc3MiOiJodHRwczovL2xpbmRlbi5hdXRoaW5nLmNuL29pZGMiLCJhdWQiOiI2MjA2Mjk0MWVmOTJhOWQxNzA5Y2Y2MDUiLCJhenAiOiI2MTdhNDM5Y2FmNThhYWZjMGY5M2NiOGUifQ.oHzKGFyUgoJ5EipyFiAJHLnfAUFwdnHK7TL3XIcVyx6Q9s1sRsTkOZnq9RTfEhqKLHagzSmFIKcqCVbtHQ19XKXRW8Iht-g46SUyhm9w6smE51T-LuWLucjVcxjRtMLLW-nqdLKS0Y0N8-lT2qAz0wqe4mtCtP0f0H71Rbd329Hm_VayzEME8StnEKZ-XKeQ0a2cjmW-3vN_em-neR691b3YChegpXqzQ1jz6iIM6utYbAkaWTJcshLTRNOxxctTWeeUxh5pjXBHhdys69FYN2h4Qp7mHT347QSzj3F2n2sgd4it2xCq4iHu6g-fHfjORt-WxcnE6b_f5ggzQ5oAIA"
+        const res = await client.add.data('Hello world!1234565', { "user": "p1", "access-token": token })
+        console.log('Added file contents:', res)
+
+        assert(res.cid == "QmebsgayqHpYSzSGZDZLu8dhkfyu1j1EBKseqit1gyvaqc", "add data error.")
+    })
+})
