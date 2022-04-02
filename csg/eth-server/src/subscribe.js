bfded8e661445b83b5b7ad9bae5310ed44ae6154
lijun <lj110119@aliyun.com>
2022-04-02 17:03:00 +0800

    add provider event for eth-server.

--- csg/eth-server/src/subscribe.js
+++ csg/eth-server/src/subscribe.js
@@ -1,11 +1,26 @@
 const args = process.argv.splice(2)
 const Web3 = require('web3');
-const web3 = new Web3(Web3.givenProvider || args[0] || "wss://speedy-nodes-nyc.moralis.io/4134993de3de6b5bca90813a/bsc/testnet/ws");
+const RINKEBY_WSS = Web3.givenProvider || args[0] || "wss://speedy-nodes-nyc.moralis.io/4134993de3de6b5bca90813a/bsc/testnet/ws";
+var provider = new Web3.providers.WebsocketProvider(RINKEBY_WSS);
+var web3 = new Web3(provider);
+
+provider.on('error', e => console.log('WS Error', e));
+provider.on('end', e => {
+    console.log('WS closed');
+    console.log('Attempting to reconnect...');
+    provider = new Web3.providers.WebsocketProvider(RINKEBY_WSS);
+
+    provider.on('connect', function () {
+        console.log('WSS Reconnected');
+    });
+    
+    web3.setProvider(provider);
+});
 const contract = args[1] || "0x0cd43FFF2a992E094E829B4b826fC67aBAe2D6E3"
 
 const producer = require('../mq/producer');
 producer.init(args[2] || "127.0.0.1:30876")
-const chainId = args[3] || "94"
+const chainId = args[3] || "97"
 
 var subscription = web3.eth.subscribe('logs', {
     address: contract,
@@ -15,9 +30,13 @@ var subscription = web3.eth.subscribe('logs', {
         console.log("error:", error)
     }
     if (result) {
+        result.chainId = chainId
+        result.createAt = Date.now()
         producer.sendWithTag(chainId, result.transactionHash, JSON.stringify(result))
         console.log("sendMQ:", chainId, JSON.stringify(result))
     }
 });
+
+
 
 console.log("subscription:", subscription);
