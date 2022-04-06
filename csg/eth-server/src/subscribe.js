ac8a03e51ee9ee420a58ac103cccfcf28c679dea
lijun <lj110119@aliyun.com>
2022-04-06 14:27:33 +0800

    update wss provider.

--- csg/eth-server/src/subscribe.js
+++ csg/eth-server/src/subscribe.js
@@ -1,26 +1,26 @@
 const args = process.argv.splice(2)
 const Web3 = require('web3');
 const RINKEBY_WSS = Web3.givenProvider || args[0] || "wss://speedy-nodes-nyc.moralis.io/4134993de3de6b5bca90813a/bsc/testnet/ws";
-var provider = new Web3.providers.WebsocketProvider(RINKEBY_WSS);
-var web3 = new Web3(provider);
-
-provider.on('error', e => console.log('WS Error', e));
-provider.on('end', e => {
-    console.log('WS closed');
-    console.log('Attempting to reconnect...');
-    provider = new Web3.providers.WebsocketProvider(RINKEBY_WSS);
-
-    provider.on('connect', function () {
-        console.log('WSS Reconnected');
-    });
-
-    web3.setProvider(provider);
-});
 const contract = args[1] || "0x0cd43FFF2a992E094E829B4b826fC67aBAe2D6E3"
-
 const producer = require('../mq/producer');
 producer.init(args[2] || "127.0.0.1:30876")
 const chainId = args[3] || "97"
+
+let options = {
+    clientConfig: {
+        keepalive: true,
+        keepaliveInterval: 60000,
+    },
+    reconnect: {
+        auto: true,
+        delay: 5000, // ms
+        maxAttempts: 5,
+        onTimeout: false
+    }
+}
+var provider = new Web3.providers.WebsocketProvider(RINKEBY_WSS, options);
+var web3 = new Web3(provider);
+subscribe();
 
 function subscribe() {
     let subscription = web3.eth.subscribe('logs', {
@@ -37,21 +37,18 @@ function subscribe() {
             console.log("sendMQ:", chainId, JSON.stringify(result))
         }
     });
-    return subscription;
+    console.log("*** subscription:", subscription);
 }
-let subscription = subscribe()
-console.log("subscription:", subscription);
 
-provider.on('error', e => console.log('WS Error', e));
-provider.on('end', e => {
-    console.log('WS closed');
-    console.log('Attempting to reconnect...');
-    provider = new Web3.providers.WebsocketProvider(RINKEBY_WSS);
+// provider.on('error', e => console.log('WS Error', e));
+// provider.on('end', e => {
+//     console.log('WS closed');
+//     console.log('Attempting to reconnect...');
+//     provider = new Web3.providers.WebsocketProvider(RINKEBY_WSS);
 
-    provider.on('connect', function () {
-        subscribe()
-        console.log('WSS Reconnected')
-    });
-
-    web3.setProvider(provider);
-});
+//     provider.on('connect', function () {
+//         console.log('WSS Reconnected');
+//     });
+    
+//     web3.setProvider(provider);
+// });
